20d7231cb1fb5ac92fc807efa3350ca616b96ee7
pedro.pasv <paventura@stefanini.com>
2022-08-09 16:01:18 +0100

    Bootstrapping receipes

---
+++ src/main/java/abn/com/receipes/ReceipesApplication.java
@@ -0,0 +1,13 @@
+package abn.com.receipes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReceipesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReceipesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/abn/com/receipes/ReceipesApplicationTests.java
@@ -0,0 +1,13 @@
+package abn.com.receipes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReceipesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
