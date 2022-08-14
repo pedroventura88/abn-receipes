2e98480888bd558be610c6faa7ce3689128976db
pedro.pasv <paventura@stefanini.com>
2022-08-14 20:31:37 +0100

    Final unit test adjustments

--- src/test/java/abn/com/receipes/core/RecipeServiceTest.java
+++ src/test/java/abn/com/receipes/core/RecipeServiceTest.java
@@ -2,7 +2,6 @@ package abn.com.receipes.core;
 
 
 import abn.com.receipes.core.db.DBMock;
-import abn.com.receipes.core.receipe.ReceipeEntity;
 import abn.com.receipes.core.receipe.ReceipeMapper;
 import abn.com.receipes.core.receipe.ReceipeRepository;
 import abn.com.receipes.core.receipe.ReceipeService;
@@ -13,17 +12,8 @@ import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageImpl;
-import org.springframework.data.domain.Pageable;
-
-import java.util.List;
-import java.util.stream.Collectors;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.*;
 
 @SpringBootTest
 public class RecipeServiceTest {
@@ -31,7 +21,7 @@ public class RecipeServiceTest {
     @Autowired
     private ReceipeService service;
 
-    @MockBean
+    @Autowired
     private ReceipeRepository repository;
 
     private DBMock mockData;
@@ -42,52 +32,39 @@ public class RecipeServiceTest {
     @BeforeEach
     void setUp() {
         mockData = new DBMock(mapper);
+        mockData.listRecipes().forEach(recipe -> repository.save(recipe));
     }
 
     @Test
     void should_get_all_available_recipes() {
-        doReturn(new PageImpl<>(mockData.listRecipes())).when(repository).findAll(any(), any(Pageable.class));
         Receipes receipes = service.getReceipes(1, 10, new Search());
         assertEquals(4, receipes.getItems().size());
-        verify(repository, times(1)).findAll(any(), any(Pageable.class));
     }
 
-//    @Test
-//    void should_get_all_available_recipes() {
-//        doReturn(new PageImpl<>(mockData.listRecipes())).when(repository).findAll(any(), any(Pageable.class));
-//        Receipes receipes = service.getReceipes(1, 10, new Search());
-//        assertEquals(4, receipes.getItems().size());
-//        verify(repository, times(1)).findAll(any(), any(Pageable.class));
-//    }
-
-//    @Test
-//    void should_get_all_available_vegetarian_recipes() {
-//        doReturn(pageAllVegetarians()).when(repository).findAll(any(), any(Pageable.class));
-//        Receipes receipes = service.getReceipes(1,10, new Search());
-//        assertEquals(2, receipes.getItems().size());
-//        verify(repository, times(1)).findAll(any(), any(Pageable.class));
-//    }
-//
-//    @Test
-//    void should_get_recipes_that_serving_4_and_have_potatoes_as_ingredient() {
-//        doReturn(pageServing4AndHavePotatoes()).when(repository).findAll(any(), any(Pageable.class));
-//        Receipes receipes = service.getReceipes(1, 10, new Search());
-//        assertEquals(1, receipes.getItems().size());
-//        assertEquals(4, receipes.getItems().get(0).getId());
-//        verify(repository, times(1)).findAll(any(), any(Pageable.class));
-//    }
-
-    private Page<ReceipeEntity> pageServing4AndHavePotatoes() {
-        List<ReceipeEntity> recipes = mockData.listRecipes().stream()
-                .filter(recipe -> recipe.getServings() == 4)
-                .filter(recipe -> recipe.getIngredients().contains("potatoes"))
-                .collect(Collectors.toList());
-        return new PageImpl<>(recipes);
+    @Test
+    void should_get_all_available_vegetarian_recipes() {
+        Receipes vegetarianReceipes = service.getReceipes(1, 10, vegetarianSearch());
+        assertEquals(2, vegetarianReceipes.getItems().size());
     }
 
-    private Page<ReceipeEntity> pageAllVegetarians() {
-        List<ReceipeEntity> receipes = mockData.listRecipes().stream().filter(recipe -> recipe.getIsVegetarian().equals(true)).collect(Collectors.toList());
-        return new PageImpl<>(receipes);
+    @Test
+    void should_get_recipes_that_serving_4_and_have_potatoes_as_ingredient() {
+        Receipes recipesServing4andHavePotatoes = service.getReceipes(1,10, serving4andHavePotatoesSearch());
+        assertEquals(1, recipesServing4andHavePotatoes.getItems().size());
+        assertEquals(4, recipesServing4andHavePotatoes.getItems().get(0).getId());
+    }
+
+    private Search vegetarianSearch() {
+        Search search = new Search();
+        search.setIsVegetarian(true);
+        return search;
+    }
+
+    private Search serving4andHavePotatoesSearch() {
+        Search search = new Search();
+        search.setServings(4);
+        search.setIngredients("potatoes");
+        return search;
     }
 
 }
